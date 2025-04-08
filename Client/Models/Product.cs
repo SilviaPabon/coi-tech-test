af80d05fb1a27eb61e523fe29a09fbc429407b0f
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2025-04-08 11:51:44 -0500

    feat(front): :sparkles: init front feat

---
+++ Client/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Client.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime DateCreation { get; set; } = DateTime.UtcNow;
+
+        public DateTime DateUpdate { get; set; } = DateTime.UtcNow;
+
+        public bool Status { get; set; }
+    }
+}
